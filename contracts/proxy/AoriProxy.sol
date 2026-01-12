a6be15f294af4430dacb63e9ae3efe16148bad6e
flooreyes <flooreyes@protonmail.com>
2026-01-12 16:05:53 -0500

    ascii

--- contracts/proxy/AoriProxy.sol
+++ contracts/proxy/AoriProxy.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.33;
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /**
- *                             @@@@@@@@@@@@
+ *                                @@@@@@@@@@@@
  *                              @@         @@@@@@                     @@@@@
  *                              @@           @@@@@                    @@@@@
  *                              @@@
